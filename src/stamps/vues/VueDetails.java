5d295c49cbde657fae05f14a154b50b587196cb3
Synnly <emanuelfernandes54@gmail.com>
2023-05-21 23:52:38 +0200

    Ajout du dino qui tourne

--- src/stamps/vues/VueDetails.java
+++ src/stamps/vues/VueDetails.java
@@ -1,5 +1,11 @@
 package stamps.vues;
 
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 import stamps.donnees.Processeur;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,12 +18,20 @@ public class VueDetails implements Observateur {
 
     protected CollectionProcesseurs collection;
     protected Image image;
+    private TranslateTransition transition;
+    private RotateTransition rotation;
 
     @FXML
     private AnchorPane vueDetails;
 
     @FXML
     private Button boutonHome, boutonPrecedent, boutonSuivant;
+
+    @FXML
+    private ImageView dino;
+
+    @FXML
+    private Pane enclosDino;
 
     public VueDetails(CollectionProcesseurs collec){
         collec.ajouterObservateur(this);
@@ -28,6 +42,30 @@ public class VueDetails implements Observateur {
     public void initialize(){
         boutonPrecedent.disableProperty().bind(collection.getIndiceProcesseurEnVueDetailsProperty().isEqualTo(0));
         boutonSuivant.disableProperty().bind(collection.getIndiceProcesseurEnVueDetailsProperty().isEqualTo(collection.getPropertyNbProcesseurs().subtract(1)));
+
+        transition = new TranslateTransition();
+        transition.setDuration(Duration.seconds(10));
+        transition.setNode(dino);
+        transition.setFromX(dino.getLayoutY());
+        transition.setCycleCount(TranslateTransition.INDEFINITE);
+        transition.setAutoReverse(true);
+        transition.setInterpolator(Interpolator.LINEAR);
+        enclosDino.widthProperty().addListener((observable, oldValue, newValue) -> {
+            transition.setToX(newValue.doubleValue() - dino.getFitWidth());
+            transition.stop();
+            rotation.stop();
+            transition.play();
+            rotation.play();
+        });
+        transition.play();
+
+        rotation = new RotateTransition();
+        rotation.setDuration(Duration.seconds(10));
+        rotation.setNode(dino);
+        rotation.setByAngle(3 * 360);
+        rotation.setCycleCount(RotateTransition.INDEFINITE);
+        rotation.setInterpolator(Interpolator.LINEAR);
+        rotation.setAutoReverse(true);
     }
 
     /**
@@ -64,6 +102,10 @@ public class VueDetails implements Observateur {
         vueDetails.setVisible(collection.estEnVueDetails());
 
         if(collection.estEnVueDetails()) {
+
+            //transition.toXProperty().bind(enclosDino.widthProperty().subtract(dino.getFitWidth()));
+            transition.play();
+            rotation.play();
 
             // S'il n'y a pas de modifications en cours, on va chercher l'image dans la collection
             if (!collection.estEnCoursDeModification()){
